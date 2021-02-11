9d5d5155afac96909a7c622fdd2d8a4c04856f40
MadErstam <erstam@gmx.de>
2021-02-11 17:02:21 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"controme adapter settings": {
+		"en": "Adapter settings for controme",
+		"de": "Adaptereinstellungen für controme",
+		"ru": "Настройки адаптера для controme",
+		"pt": "Configurações do adaptador para controme",
+		"nl": "Adapterinstellingen voor controme",
+		"fr": "Paramètres d'adaptateur pour controme",
+		"it": "Impostazioni dell'adattatore per controme",
+		"es": "Ajustes del adaptador para controme",
+		"pl": "Ustawienia adaptera dla controme",
+		"zh-cn": "controme的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
